3b9cece3cc66e495dfec87571f6850e654d903ca
Andrea Rizzello <rizzell0andr3a@gmail.com>
2017-11-20 13:47:44 +0100

    nothing

--- src/ft_checkvalidfile.c
+++ src/ft_checkvalidfile.c
@@ -6,13 +6,24 @@
 /*   By: arizzell <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/19 17:07:07 by arizzell          #+#    #+#             */
-/*   Updated: 2017/11/19 17:07:09 by arizzell         ###   ########.fr       */
+/*   Updated: 2017/11/20 13:33:34 by arizzell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fillit.h"
 #define BUFFER_SZ 4096
 
+static char *ft_returnstring(const char *bf, int nlines)
+{
+	while (bf[0] != '\n' & bf[1] != '\n')
+	{
+		if (*bf == '\n')
+			nlines++;
+		if (*bf == '#' )
+			nlines++;
+		bf++;
+	}
+}
 
 int	ft_checkvalidfile(int fd)
 {
@@ -27,23 +38,12 @@ int	ft_checkvalidfile(int fd)
 	bf = (char *)malloc(sizeof(char) * BUFFER_SZ + 1);
 	ret = (char **)malloc(sizeof(char *) * 4);
 	reader = read(fd, bf, BUFFER_SZ);
-	while (reader && nlines <=4)
-	{
-		if (*reader == '\n')
-			nlines++;
-		if (*reader ==)
-		//TODO: use a "compass" to understand how to put the arrows... ^v><
-	}
-	return 0;
-}
-
-int main()
-{
-	int fd = 0;
-
-	fd = open("tetris", O_RDONLY);
-	if (fd == -1)
+	if (reader == 0)
 		exit(0);
-	ft_checkvalidfile(fd);
+	ret = (char **) ft_returnstring(bf, 0); //TODO: FIX
+	free(*ret);
+	free(ret);
+	free(bf);
 	return 0;
 }
+
